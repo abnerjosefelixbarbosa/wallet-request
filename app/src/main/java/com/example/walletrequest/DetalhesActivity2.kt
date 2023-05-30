9b3a7d7f74b565fab0e1a3a68931be7d1a5106db
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-05-30 16:12:36 -0300

    commited by Abner

---
+++ app/src/main/java/com/example/walletrequest/DetalhesActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.walletrequest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes2)
+    }
+}
