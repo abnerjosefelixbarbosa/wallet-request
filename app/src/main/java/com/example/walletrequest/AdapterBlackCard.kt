5b0f299ba25e0a046d5bec2190e5f798b4ab551f
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-05-31 19:10:42 -0300

    commited by Abner

---
+++ app/src/main/java/com/example/walletrequest/AdapterBlackCard.kt
@@ -0,0 +1,7 @@
+package com.example.walletrequest
+
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.ads.mediationtestsuite.viewmodels.ItemViewHolder
+
+class Adapter(var modelos: List<String>, var colors: List<String>): RecyclerView.Adapter<ItemViewHolder>() {
+}
---
+++ app/src/main/java/com/example/walletrequest/ItemViewHolderBlackCard.kt
@@ -0,0 +1,4 @@
+package com.example.walletrequest
+
+class ItemViewHolderBlackCard {
+}
